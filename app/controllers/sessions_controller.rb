c2bb670b66411ca000de71013bb8f82be7be0200
Pankaj Bagdare <bagdarepankaj@gmail.com>
2022-06-08 13:49:27 +0530

    Few routes changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,25 +9,25 @@ class SessionsController < ApplicationController
     begin
       if @_current_user && @_current_user.authenticate(params[:password])
         session[:current_user_id] = @_current_user.id
-        redirect_to root_path, notice: 'Successfully logged in'
+        redirect_to home_path, notice: 'User successfully logged in'
       else
-        redirect_to login_path, notice: 'Invalid email or password'
+        redirect_to root_path, notice: 'Invalid email or password'
       end
     rescue => e
-      redirect_to login_path, notice: 'Something went wrong please try again'
+      redirect_to root_path, notice: 'Something went wrong please try again'
     end
   end
 
   def destroy
     session[:current_user_id] = nil
-    redirect_to login_path, notice: 'User logged out successfully'
+    redirect_to root_path, notice: 'User logged out successfully'
   end
 
   private
 
   def already_logged_in
     if logged_in?
-      redirect_to root_path, notice: 'You are already logged in'
+      redirect_to home_path, notice: 'You are already logged in'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   resources :users
   get 'home', to: 'home#index'
-  get 'login', to: 'sessions#new'
+  # get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
   post 'message', to: 'messages#create'
-  root 'home#index'
+
+  root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
